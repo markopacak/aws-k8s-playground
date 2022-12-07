64a917f9ef8b8e689fbef6f5364cae2aede0da45
Marko Pacak <info@markopacak.com>
2022-12-07 21:44:47 +0100

    feat: finish lambda

--- src/lambda_cloudwatch_ec_shutdown/lambda.py
+++ src/lambda_cloudwatch_ec_shutdown/lambda.py
@@ -2,14 +2,19 @@ import json
 
 import boto3
 
+ec2 = boto3.client("ec2")
+
+tag_filter = [{"Name": "tag:Project", "Values": ["k8s-playground"]}]
+
 
 def lambda_handler(event, context):
-    ec2 = boto3.client("ec2")
-    resp = ec2.describe_instances(Filters=[{"Name": "tag:k8s-playground"}])
-    print(resp)
+    resp = ec2.describe_instances(Filters=tag_filter)
+
     ids = []
-    resp = ec2.stop_instances(InstanceIds=ids)  # TODO
-    print(resp)
+    for res in resp["Reservations"]:
+        ids.extend(list(map(lambda i: i["InstanceId"], res["Instances"])))
+
+    resp = ec2.stop_instances(InstanceIds=ids)
     return {
         "statusCode": 200,
         "body": json.dumps(f"Successfully shutdown machines {ids}"),
