85579e2e3e1c98c2609c9d0d75b3307a653bea3b
Marko Pacak <info@markopacak.com>
2022-12-10 11:04:55 +0100

    refactor: rewrite lambda

--- src/lambda_cloudwatch_ec_shutdown/lambda.py
+++ src/lambda_cloudwatch_ec_shutdown/lambda.py
@@ -9,13 +9,12 @@ tag_filter = [{"Name": "tag:Project", "Values": ["k8s-playground"]}]
 
 def lambda_handler(event, context):
     resp = ec2.describe_instances(Filters=tag_filter)
-
-    ids = []
-    for res in resp["Reservations"]:
-        ids.extend(list(map(lambda i: i["InstanceId"], res["Instances"])))
+    ids = [
+        i["InstanceId"] for r in resp["Reservations"] for i in r["Instances"]
+    ]
 
     resp = ec2.stop_instances(InstanceIds=ids)
     return {
-        "statusCode": 200,
+        "statusCode": resp["ResponseMetadata"]["HTTPStatusCode"],
         "body": json.dumps(f"Successfully shutdown machines {ids}"),
     }
